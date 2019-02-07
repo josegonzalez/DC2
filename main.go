03179e009adfaaa88f984243cac792bd34e2517c
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-02-07 15:46:27 -0500

    feat: add full compatibility with the old dc2 node service
    - switch to github releases for fetching the latest release
    - only show the primary ethernet mac address
    - add a small amount of logging

--- main.go
+++ main.go
@@ -2,16 +2,95 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 type response struct {
-	Version      string            `json:"version"`
-	Hostname     string            `json:"hostname"`
-	IPAddress    string            `json:"ip"`
-	MacAddresses map[string]string `json:"mac_addresses"`
+	CallHomeResponse int    `json:"callHomeResponse"`
+	Hostname         string `json:"hostname"`
+	IPAddress        string `json:"ip"`
+	LatestVersion    string `json:"latestVersion"`
+	MacAddress       string `json:"MAC"`
+	Version          string `json:"version"`
+}
+
+type githubRelease struct {
+	URL             string `json:"url"`
+	AssetsURL       string `json:"assets_url"`
+	UploadURL       string `json:"upload_url"`
+	HTMLURL         string `json:"html_url"`
+	ID              int    `json:"id"`
+	NodeID          string `json:"node_id"`
+	TagName         string `json:"tag_name"`
+	TargetCommitish string `json:"target_commitish"`
+	Name            string `json:"name"`
+	Draft           bool   `json:"draft"`
+	Author          struct {
+		Login             string `json:"login"`
+		ID                int    `json:"id"`
+		NodeID            string `json:"node_id"`
+		AvatarURL         string `json:"avatar_url"`
+		GravatarID        string `json:"gravatar_id"`
+		URL               string `json:"url"`
+		HTMLURL           string `json:"html_url"`
+		FollowersURL      string `json:"followers_url"`
+		FollowingURL      string `json:"following_url"`
+		GistsURL          string `json:"gists_url"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		OrganizationsURL  string `json:"organizations_url"`
+		ReposURL          string `json:"repos_url"`
+		EventsURL         string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"author"`
+	Prerelease  bool      `json:"prerelease"`
+	CreatedAt   time.Time `json:"created_at"`
+	PublishedAt time.Time `json:"published_at"`
+	Assets      []struct {
+		URL      string `json:"url"`
+		ID       int    `json:"id"`
+		NodeID   string `json:"node_id"`
+		Name     string `json:"name"`
+		Label    string `json:"label"`
+		Uploader struct {
+			Login             string `json:"login"`
+			ID                int    `json:"id"`
+			NodeID            string `json:"node_id"`
+			AvatarURL         string `json:"avatar_url"`
+			GravatarID        string `json:"gravatar_id"`
+			URL               string `json:"url"`
+			HTMLURL           string `json:"html_url"`
+			FollowersURL      string `json:"followers_url"`
+			FollowingURL      string `json:"following_url"`
+			GistsURL          string `json:"gists_url"`
+			StarredURL        string `json:"starred_url"`
+			SubscriptionsURL  string `json:"subscriptions_url"`
+			OrganizationsURL  string `json:"organizations_url"`
+			ReposURL          string `json:"repos_url"`
+			EventsURL         string `json:"events_url"`
+			ReceivedEventsURL string `json:"received_events_url"`
+			Type              string `json:"type"`
+			SiteAdmin         bool   `json:"site_admin"`
+		} `json:"uploader"`
+		ContentType        string    `json:"content_type"`
+		State              string    `json:"state"`
+		Size               int       `json:"size"`
+		DownloadCount      int       `json:"download_count"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		BrowserDownloadURL string    `json:"browser_download_url"`
+	} `json:"assets"`
+	TarballURL string      `json:"tarball_url"`
+	ZipballURL string      `json:"zipball_url"`
+	Body       interface{} `json:"body"`
 }
 
 // Version for sshd-config
@@ -39,30 +118,58 @@ func getIPAddress() string {
 	return ""
 }
 
-func getMacAddresses() map[string]string {
-	as := make(map[string]string)
+func getMacAddress() string {
 	ifas, err := net.Interfaces()
 	if err != nil {
-		return as
+		return ""
 	}
 
 	for _, ifa := range ifas {
 		n := ifa.Name
 		a := ifa.HardwareAddr.String()
-		if a != "" {
-			as[n] = a
+		isEth := strings.HasPrefix(n, "en") || strings.HasPrefix(n, "eth")
+		if a != "" && isEth {
+			return a
 		}
 	}
-	return as
+	return ""
+}
+
+func getJson(url string, target interface{}) error {
+	client := &http.Client{Timeout: 10 * time.Second}
+	r, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
 func main() {
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+
+	logger.Println("Fetching latest release")
+
+	release := &githubRelease{}
+	err := getJson("http://api.github.com/repos/josegonzalez/dc2/releases/latest", release)
+	latestVersion := "unknown"
+	callHomeResponse := 404
+	if err == nil {
+		latestVersion = strings.TrimPrefix(release.TagName, "v")
+		callHomeResponse = 200
+	}
+
+	logger.Println("Server is starting")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		resp := response{
-			Hostname:     getHostname(),
-			IPAddress:    getIPAddress(),
-			MacAddresses: getMacAddresses(),
-			Version:      Version,
+			Hostname:         getHostname(),
+			IPAddress:        getIPAddress(),
+			MacAddress:       getMacAddress(),
+			Version:          Version,
+			LatestVersion:    latestVersion,
+			CallHomeResponse: callHomeResponse,
 		}
 		w.Header().Set("Content-Type", "application/json")
 		enc := json.NewEncoder(w)
@@ -70,5 +177,12 @@ func main() {
 		enc.Encode(resp)
 	})
 
-	http.ListenAndServe(":8765", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8765"
+	}
+
+	listenAddr := fmt.Sprintf(":%s", port)
+	logger.Println("Server is ready to handle requests at", listenAddr)
+	http.ListenAndServe(listenAddr, nil)
 }
